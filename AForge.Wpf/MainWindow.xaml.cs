24a933910d20273d1f928e54506b51bd92c5683d
Aren Gariki Zohranyan <arenz@rambler.ruz>
2019-07-21 17:56:02 +0400

    asd

---
+++ AForge.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace AForge.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        #region Public properties
+
+        public ObservableCollection<FilterInfo> VideoDevices { get; set; }
+
+        public FilterInfo CurrentDevice
+        {
+            get { return _currentDevice; }
+            set { _currentDevice = value; this.OnPropertyChanged("CurrentDevice"); }
+        }
+        private FilterInfo _currentDevice;
+
+        #endregion
+
+
+        #region Private fields
+
+        private IVideoSource _videoSource;
+
+        #endregion
+
+
+        static int sendPort=9876;
+        static int recievePort=9877;
+        const string myIp="192.168.0.11";
+        const string recieverIp = "192.168.0.12";
+
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(myIp), sendPort);
+        IPEndPoint receiveEndPoint = new IPEndPoint(IPAddress.Any, recievePort);
+        Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Socket receiveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        byte[] sendBuffer = new byte[100000];
+        byte[] receiveBuffer = new byte[100000];
+        
+		private void SettingConnection()
+        {
+            sendBuffer = new byte[100000];
+            sendSocket.Connect(endPoint);
+        }
+        private void GettingConnection()
+        {
+            receiveBuffer = new byte[100000];
+            receiveSocket.Bind(endPoint);
+            receiveSocket.Listen(5);
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            GetVideoDevices();
+            this.Closing += MainWindow_Closing;
+
+            Thread setConnection = new Thread(SettingConnection);
+            Thread getConnection = new Thread(GettingConnection);
+            setConnection.Start();
+            getConnection.Start();
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopCamera();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            StartCamera();
+        }
+
+        private void video_NewFrame(object sender, Video.NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+                BitmapImage bi;
+                using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+                {
+                    bi = bitmap.ToBitmapImage();
+                }
+                bi.Freeze(); // avoid cross thread operations and prevents leaks
+                MemoryStream memoryStream = new MemoryStream(sendBuffer);
+
+                Dispatcher.BeginInvoke(new ThreadStart(delegate { videoPlayer.Source = bi; sendSocket.Send(sendBuffer); }));
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Error on _videoSource_NewFrame:\n" + exc.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                StopCamera();
+            }
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            StopCamera();
+        }
+
+        private void GetVideoDevices()
+        {
+            VideoDevices = new ObservableCollection<FilterInfo>();
+            foreach (FilterInfo filterInfo in new FilterInfoCollection(FilterCategory.VideoInputDevice))
+            {
+                VideoDevices.Add(filterInfo);
+            }
+            if (VideoDevices.Any())
+            {
+                CurrentDevice = VideoDevices[0];
+            }
+            else
+            {
+                MessageBox.Show("No video sources found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void StartCamera()
+        {
+            if (CurrentDevice != null)
+            {
+                _videoSource = new VideoCaptureDevice(CurrentDevice.MonikerString);
+                _videoSource.NewFrame += video_NewFrame;
+                _videoSource.Start();
+            }
+        }
+
+        private void StopCamera()
+        {
+            if (_videoSource != null && _videoSource.IsRunning)
+            {
+                _videoSource.SignalToStop();
+                _videoSource.NewFrame -= new NewFrameEventHandler(video_NewFrame);
+            }
+        }
+
+        #region INotifyPropertyChanged members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        } 
+
+        #endregion
+    }
+}
